276293a1ba0dc946b1c7534757d6a44b181d2699
Jason Cleveland <jasonncleveland@gmail.com>
2023-01-01 19:42:27 -0500

    Add solution to 2015 day4 part 2

---
+++ 2015/day4/part2/TheIdealStockingStuffer.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+class TheIdealStockingStuffer
+{
+  static void Main(string[] args)
+  {
+    if (args.Length > 0)
+    {
+      string fileName = args[0];
+      if (File.Exists(fileName))
+      {
+        string[] lines = File.ReadAllLines(fileName);
+
+        foreach (string line in lines)
+        {
+          int number = 0;
+          HashAlgorithm mD5 = MD5.Create();
+
+          while (true)
+          {
+            byte[] inputBytes = Encoding.ASCII.GetBytes($"{line}{number}");
+            byte[] hash = mD5.ComputeHash(inputBytes);
+            string hashString = BitConverter.ToString(hash).Replace("-", String.Empty);
+            if (hashString.StartsWith("000000"))
+            {
+              Console.WriteLine(hashString);
+              break;
+            }
+            number++;
+          }
+
+          Console.WriteLine($"Lowest number: {number}");
+        }
+      }
+    }
+  }
+}
