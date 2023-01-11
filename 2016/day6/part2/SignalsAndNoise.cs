0b7ce188951b1effb92b29985bc6eeae2146021b
Jason Cleveland <jasonncleveland@gmail.com>
2023-01-11 17:44:58 -0500

    Add solution to 2016 day 6 part 2

---
+++ 2016/day6/part2/SignalsAndNoise.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class SignalsAndNoise
+{
+  static void Main(string[] args)
+  {
+    if (args.Length > 0)
+    {
+      string fileName = args[0];
+      if (File.Exists(fileName))
+      {
+        string[] lines = File.ReadAllLines(fileName);
+
+        List<List<LetterData>> letterCounts = new List<List<LetterData>>();
+        foreach (string line in lines)
+        {
+          for (int i = 0; i < line.Length; i++)
+          {
+            char letter = line[i];
+            if (letterCounts.Count < i + 1)
+            {
+              letterCounts.Add(new List<LetterData>());
+            }
+
+            if (letterCounts[i].FindIndex(l => l.Letter == letter) < 0)
+            {
+              letterCounts[i].Add(new LetterData(letter));
+            }
+            LetterData letterData = letterCounts[i].Find(l => l.Letter == letter);
+            letterData.Count++;
+          }
+        }
+
+        List<char> message = new List<char>();
+        foreach (List<LetterData> column in letterCounts)
+        {
+          column.Sort((a, b) => {
+            int countResult = a.Count - b.Count;
+            int letterResult = a.Letter - b.Letter;
+            return countResult == 0 ? letterResult : countResult;
+          });
+          message.Add(column[0].Letter);
+        }
+        string correctedMessage = new string(message.ToArray());
+        Console.WriteLine($"Error-corrected message: {correctedMessage}");
+      }
+    }
+  }
+}
+
+class LetterData
+{
+  public char Letter { get; set; }
+  public int Count { get; set; }
+
+  public LetterData(char letter)
+  {
+    Letter = letter;
+    Count = 0;
+  }
+}
