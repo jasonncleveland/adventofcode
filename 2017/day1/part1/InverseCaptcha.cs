61daed421da10e09c79cc22f5af170db4e613eda
Jason Cleveland <jasonncleveland@gmail.com>
2023-06-04 15:15:10 -0400

    Add solution to 2017 day 1 part 1

---
+++ 2017/day1/part1/InverseCaptcha.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+
+class InverseCaptcha
+{
+  static void Main(string[] args)
+  {
+    if (args.Length > 0)
+    {
+      string fileName = args[0];
+      if (File.Exists(fileName))
+      {
+        string[] lines = File.ReadAllLines(fileName);
+
+        foreach (string line in lines)
+        {
+          int totalSum = 0;
+          for (int i = 0, s = line.Length; i < s; i++)
+          {
+            int firstItem = int.Parse(line[i].ToString());
+            int secondItem = int.Parse(line[(i + 1) % s].ToString());
+            if (firstItem == secondItem)
+            {
+              totalSum += firstItem;
+            }
+          }
+          Console.WriteLine($"Total sum: {totalSum}");
+        }
+      }
+    }
+  }
+}
