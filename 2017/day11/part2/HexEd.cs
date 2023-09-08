8a88d5465342dbaedb802ccddc2bc426ea5e4b2a
Jason Cleveland <jasonncleveland@gmail.com>
2023-09-08 16:29:02 -0400

    Add solution to 2017 day 11 part 2

---
+++ 2017/day11/part2/HexEd.cs
@@ -0,0 +1,69 @@
+using System;
+using System.IO;
+
+/**
+  * Hex math taken from https://www.redblobgames.com/grids/hexagons/
+  */
+class HexEd
+{
+  static void Main(string[] args)
+  {
+    if (args.Length > 0)
+    {
+      string fileName = args[0];
+      if (File.Exists(fileName))
+      {
+        string[] lines = File.ReadAllLines(fileName);
+
+        foreach (string line in lines)
+        {
+          string[] directions = line.Split(',');
+
+          int farthestDistanceReached = int.MinValue;
+          int q = 0, s = 0, r = 0;
+          foreach (string direction in directions)
+          {
+            switch (direction)
+            {
+              case "nw":
+                q -= 1;
+                s += 1;
+                break;
+              case "n":
+                s += 1;
+                r -= 1;
+                break;
+              case "ne":
+                q += 1;
+                r -= 1;
+                break;
+              case "sw":
+                q -= 1;
+                r += 1;
+                break;
+              case "s":
+                s -= 1;
+                r += 1;
+                break;
+              case "se":
+                q += 1;
+                s -= 1;
+                break;
+              default:
+                throw new Exception($"Invalid direction found: {direction}");
+            }
+            // Calculate Manhatten Distance at each step to determine farthest distance reached
+            int manhattenDistance = (Math.Abs(q) + Math.Abs(s) + Math.Abs(r)) / 2;
+            if (manhattenDistance > farthestDistanceReached)
+            {
+              farthestDistanceReached = manhattenDistance;
+            }
+          }
+          int currentManhattenDistance = (Math.Abs(q) + Math.Abs(s) + Math.Abs(r)) / 2;
+          Console.WriteLine($"Current Manhatten Distance: {currentManhattenDistance}");
+          Console.WriteLine($"Farthest Manhatten Distance Reached: {farthestDistanceReached}");
+        }
+      }
+    }
+  }
+}
