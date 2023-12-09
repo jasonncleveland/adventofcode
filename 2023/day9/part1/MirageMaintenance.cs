faebd59e2561a3282dd709214cd51b08f05764e6
Jason Cleveland <jasonncleveland@gmail.com>
2023-12-09 01:59:03 -0500

    Add solution to 2023 day 9 part 1

---
+++ 2023/day9/part1/MirageMaintenance.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+class MirageMaintenance
+{
+  static void Main(string[] args)
+  {
+    if (args.Length > 0)
+    {
+      string fileName = args[0];
+      if (File.Exists(fileName))
+      {
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+
+        string[] lines = File.ReadAllLines(fileName);
+
+        int total = 0;
+        foreach (string line in lines)
+        {
+          string[] lineParts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+          List<int> lineNumbers = new List<int>();
+          foreach (string number in lineParts)
+          {
+            lineNumbers.Add(int.Parse(number));
+          }
+          int nextValue = getNextValue(lineNumbers);
+          total += nextValue;
+        }
+        Console.WriteLine($"Total value: {total}");
+
+        stopWatch.Stop();
+        Console.WriteLine($"Elapsed Time: {stopWatch.ElapsedMilliseconds} ms");
+      }
+    }
+  }
+
+  static int getNextValue(List<int> numbers)
+  {
+    List<int> differences = new List<int>();
+    for (int i = 1; i < numbers.Count; i++)
+    {
+      differences.Add(numbers[i] - numbers[i - 1]);
+    }
+    if (differences.All(difference => difference == 0))
+    {
+      return numbers.Last();
+    }
+    else
+    {
+      return numbers.Last() + getNextValue(differences);
+    }
+  }
+}
