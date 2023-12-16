ee75d430b6bfeacfd32763118339ebc9fb9436cd
Jason Cleveland <jasonncleveland@gmail.com>
2023-12-16 13:19:01 -0500

    Refactor 2023 day 16
    Use threading for part 2 brute force

--- 2023/day16/part2/TheFloorWillBeLava.cs
+++ 2023/day16/part2/TheFloorWillBeLava.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Threading;
 
 class TheFloorWillBeLava
 {
@@ -24,24 +25,50 @@ class TheFloorWillBeLava
           grid.Add(new List<char>(line));
         }
 
+        // Assume the grid is a valid rectangle
+        int maxRow = grid.Count;
+        int maxColumn = grid[0].Count;
         int maxTotal = int.MinValue;
-        for (int row = 0; row < grid.Count; row++)
+        List<Thread> threads = new List<Thread>();
+        for (int row = 0; row < maxRow; row++)
         {
           // Check all left side entries
-          int leftTotal = traverseGrid(grid, row, 0, Direction.RIGHT);
-          if (leftTotal > maxTotal) maxTotal = leftTotal;
+          Thread leftThread = new Thread(() => {
+            int leftTotal = traverseGrid(grid, row, 0, Direction.RIGHT);
+            if (leftTotal > maxTotal) maxTotal = leftTotal;
+          });
+          leftThread.Start();
+          threads.Add(leftThread);
+
           // Check all right side entries
-          int rightTotal = traverseGrid(grid, row, grid[row].Count - 1, Direction.LEFT);
-          if (rightTotal > maxTotal) maxTotal = rightTotal;
+          Thread rightThread = new Thread(() => {
+            int rightTotal = traverseGrid(grid, row, maxColumn - 1, Direction.LEFT);
+            if (rightTotal > maxTotal) maxTotal = rightTotal;
+          });
+          rightThread.Start();
+          threads.Add(rightThread);
         }
-        for (int column = 0; column < grid[0].Count; column++)
+        for (int column = 0; column < maxColumn; column++)
         {
           // Check all top side entries
-          int topTotal = traverseGrid(grid, 0, column, Direction.DOWN);
-          if (topTotal > maxTotal) maxTotal = topTotal;
+          Thread topThread = new Thread(() => {
+            int topTotal = traverseGrid(grid, 0, column, Direction.DOWN);
+            if (topTotal > maxTotal) maxTotal = topTotal;
+          });
+          topThread.Start();
+          threads.Add(topThread);
+
           // Check all bottom side entries
-          int bottomTotal = traverseGrid(grid, grid.Count - 1, column, Direction.UP);
-          if (bottomTotal > maxTotal) maxTotal = bottomTotal;
+          Thread bottomThread = new Thread(() => {
+            int bottomTotal = traverseGrid(grid, maxRow, column, Direction.UP);
+            if (bottomTotal > maxTotal) maxTotal = bottomTotal;
+          });
+          bottomThread.Start();
+          threads.Add(bottomThread);
+        }
+        foreach (Thread thread in threads)
+        {
+          thread.Join();
         }
         Console.WriteLine($"Total value: {maxTotal}");
 
