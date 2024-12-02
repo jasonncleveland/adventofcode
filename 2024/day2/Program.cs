05915356ca63b43f655142f06d89c1d6a7c1c6ff
Jason Cleveland <jasonncleveland@gmail.com>
2024-12-02 00:54:32 -0500

    Add solution to 2024 day 2 part 1

--- 2024/day2/Program.cs
+++ 2024/day2/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 
@@ -20,6 +21,7 @@ public class Program
                 stopWatch.Stop();
                 Console.WriteLine($"Part 1: {part1} ({stopWatch.ElapsedMilliseconds} ms)");
 
+                stopWatch.Reset();
                 stopWatch.Start();
                 long part2 = SolvePart2(lines);
                 stopWatch.Stop();
@@ -40,7 +42,17 @@ public class Program
     {
         long total = 0;
 
-        // TODO: Implement logic to solve part 1
+        List<List<int>> reports = parseInput(lines);
+        foreach (List<int> report in reports)
+        {
+            Queue<int> reportQueue = new(report);
+            if (reportQueue.Count < 2)
+            {
+                Console.WriteLine($"Found report with <2 levels: {string.Join(",", reportQueue)}");
+                continue;
+            }
+            if (checkReport(reportQueue)) total++;
+        }
 
         return total;
     }
@@ -52,5 +64,60 @@ public class Program
         // TODO: Implement logic to solve part 2
 
         return total;
+    }
+    
+    static List<List<int>> parseInput(string[] lines)
+    {
+        List<List<int>> rows = new();
+        foreach (string line in lines)
+        {
+            if (line.StartsWith("#")) continue;
+            List<int> row = new();
+            string[] lineParts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            foreach (string item in lineParts)
+            {
+                row.Add(int.Parse(item));
+            }
+            rows.Add(row);
+        }
+        return rows;
+    }
+
+    static bool checkReport(Queue<int> reportQueue)
+    {
+        bool isValid = true;
+        int previousLevel = reportQueue.Dequeue();
+        int currentLevel = reportQueue.Dequeue();
+        bool isIncreasing = currentLevel > previousLevel;
+
+        while (true)
+        {
+            int diff = Math.Abs(currentLevel - previousLevel);
+
+            // Difference must be between 1 and 3
+            if (diff < 1 || diff > 3)
+            {
+                isValid = false;
+                break;
+            }
+
+            // Must be always increasing or always decreasing
+            if (isIncreasing && currentLevel < previousLevel || !isIncreasing && currentLevel > previousLevel)
+            {
+                isValid = false;
+                break;
+            }
+
+            // Stop when there are no items left to check
+            if (reportQueue.Count == 0)
+            {
+                break;
+            }
+
+            // Get the next item to check
+            previousLevel = currentLevel;
+            currentLevel = reportQueue.Dequeue();
+        }
+        return isValid;
     }
 }
