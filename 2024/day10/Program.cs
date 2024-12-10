eb7e3d4b8561e01b345e4a77b4d6daa64a6831f0
Jason Cleveland <jasonncleveland@gmail.com>
2024-12-10 00:56:41 -0500

    Add solution to 2024 day 10 part 2

--- 2024/day10/Program.cs
+++ 2024/day10/Program.cs
@@ -66,7 +66,17 @@ public class Program
     {
         long total = 0;
 
-        // TODO: Implement logic to solve part 2
+        List<(int, int)> trailHeads = new();
+        for (int row = 0; row < lines.Length; row++)
+        {
+            for (int column = 0; column < lines[row].Length; column++)
+            {
+                if (lines[row][column] == '0')
+                {
+                    total += findUniqueTrailsRec(lines, row, column);
+                }
+            }
+        }
 
         return total;
     }
@@ -96,6 +106,31 @@ public class Program
         }
 
         return tops;
+    }
+
+    static int findUniqueTrailsRec(string[] map, int row, int column)
+    {
+        int total = 0;
+
+        if (map[row][column] == '9')
+        {
+            return 1;
+        }
+
+        foreach (Direction direction in Enum.GetValues(typeof(Direction)))
+        {
+            (int nextRow, int nextColumn) = direction.GetNextPosition(row, column);
+            if (nextRow >= 0 && nextRow < map.Length && nextColumn >= 0 && nextColumn < map[nextRow].Length)
+            {
+                // Only check the next height if it is 1 greater than the current height
+                if (map[nextRow][nextColumn] - map[row][column] == 1)
+                {
+                    total += findUniqueTrailsRec(map, nextRow, nextColumn);
+                }
+            }
+        }
+
+        return total;
     }
 }
 
