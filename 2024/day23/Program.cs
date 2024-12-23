441b8e0f629a5cd87840fe11a94543b003bee8e3
Jason Cleveland <jasonncleveland@gmail.com>
2024-12-23 00:11:32 -0500

    Add template for 2024 day 23

---
+++ 2024/day23/Program.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length > 0)
+        {
+            string fileName = args[0];
+            if (File.Exists(fileName))
+            {
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                string[] lines = File.ReadAllLines(fileName);
+                stopWatch.Stop();
+                Console.WriteLine($"File read ({stopWatch.Elapsed.TotalMilliseconds} ms)");
+
+                Stopwatch part1Timer = new Stopwatch();
+                part1Timer.Start();
+                long part1 = SolvePart1(lines);
+                part1Timer.Stop();
+                Console.WriteLine($"Part 1: {part1} ({part1Timer.Elapsed.TotalMilliseconds} ms)");
+
+                Stopwatch part2Timer = new Stopwatch();
+                part2Timer.Start();
+                long part2 = SolvePart2(lines);
+                part2Timer.Stop();
+                Console.WriteLine($"Part 2: {part2} ({part2Timer.Elapsed.TotalMilliseconds} ms)");
+            }
+            else
+            {
+                throw new ArgumentException("Invalid file name provided. Please provide a valid file name.");
+            }
+        }
+        else
+        {
+            throw new ArgumentException("Input data file name not provided. Please provide the file name as an argument: dotnet run <file-name>");
+        }
+    }
+
+    static long SolvePart1(string[] lines)
+    {
+        long total = 0;
+
+        // TODO: Implement logic to solve part 1
+
+        return total;
+    }
+
+    static long SolvePart2(string[] lines)
+    {
+        long total = 0;
+
+        // TODO: Implement logic to solve part 2
+
+        return total;
+    }
+}
