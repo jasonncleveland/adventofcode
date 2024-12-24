9fae18f2bfee1c2a5a682e6f2364a93a03072fd3
Jason Cleveland <jasonncleveland@gmail.com>
2024-12-24 08:13:07 -0500

    Add solution to 2024 day 24 part 1

--- 2024/day24/Program.cs
+++ 2024/day24/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 
@@ -44,9 +45,35 @@ public class Program
     {
         long total = 0;
 
-        // TODO: Implement logic to solve part 1
+        (Dictionary<string, long> wires, Queue<(string left, string operation, string right, string output)> instructions) = ParseInput(lines);
 
-        return total;
+        while (instructions.Count > 0)
+        {
+            (string left, string operation, string right, string output) instruction = instructions.Dequeue();
+
+            if (!wires.ContainsKey(instruction.left) || !wires.ContainsKey(instruction.right))
+            {
+                instructions.Enqueue(instruction);
+                continue;
+            }
+
+            switch (instruction.operation)
+            {
+                case "AND":
+                    wires[instruction.output] = wires[instruction.left] & wires[instruction.right];
+                    break;
+                case "OR":
+                    wires[instruction.output] = wires[instruction.left] | wires[instruction.right];
+                    break;
+                case "XOR":
+                    wires[instruction.output] = wires[instruction.left] ^ wires[instruction.right];
+                    break;
+                default:
+                    throw new Exception($"Invalid operation received: {instruction.operation}");
+            }
+        }
+
+        return GetWireValue(wires, 'z');
     }
 
     static long SolvePart2(string[] lines)
@@ -54,6 +81,63 @@ public class Program
         long total = 0;
 
         // TODO: Implement logic to solve part 2
+
+        return total;
+    }
+
+    static (Dictionary<string, long>, Queue<(string left, string operation, string right, string output)>) ParseInput(string[] lines)
+    {
+        Dictionary<string, long> wires = new();
+        Queue<(string left, string operation, string right, string output)> instructions = new();
+
+        bool readingInstructons = false;
+        foreach (string line in lines)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                readingInstructons = true;
+                continue;
+            }
+
+            if (readingInstructons)
+            {
+                string[] lineParts = line.Split(" -> ");
+                string[] instructionParts = lineParts[0].Split(' ');
+                string output = lineParts[1];
+                string operation = instructionParts[1];
+                string left = instructionParts[0];
+                string right = instructionParts[2];
+                instructions.Enqueue((left, operation, right, output));
+            }
+            else
+            {
+                string[] lineParts = line.Split(':');
+                string name = lineParts[0];
+                long value = long.Parse(lineParts[1]);
+                wires[name] = value;
+            }
+        }
+
+        return (wires, instructions);
+    }
+
+    static long GetWireValue(Dictionary<string, long> wires, char group)
+    {
+        long total = 0;
+
+        for (long n = 0;; n++)
+        {
+            string key = $"{group}{n.ToString("D2")}";
+            if (wires.ContainsKey(key))
+            {
+                long value = wires[key];
+                total += (long) Math.Pow(2, n) * value;
+            }
+            else
+            {
+                break;
+            }
+        }
 
         return total;
     }
