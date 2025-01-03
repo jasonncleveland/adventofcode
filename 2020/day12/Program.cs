0463789f879f3542db6dc7be667e3c012b850a99
Jason Cleveland <jasonncleveland@gmail.com>
2025-01-03 17:31:24 -0500

    Add solution to 2020 day 12 part 1

--- 2020/day12/Program.cs
+++ 2020/day12/Program.cs
@@ -42,11 +42,48 @@ public class Program
 
     static long SolvePart1(string[] lines)
     {
-        long total = 0;
+        int x = 0;
+        int y = 0;
 
-        // TODO: Implement logic to solve part 1
+        Direction direction = Direction.East;
 
-        return total;
+        foreach (string line in lines)
+        {
+            char operation = line[0];
+            int number = int.Parse(line.Substring(1));
+
+            switch (operation)
+            {
+                case 'N':
+                    y -= number;
+                    break;
+                case 'S':
+                    y += number;
+                    break;
+                case 'W':
+                    x -= number;
+                    break;
+                case 'E':
+                    x += number;
+                    break;
+                case 'L':
+                case 'R':
+                    for (int i = 0; i < number / 90; i++)
+                    {
+                        direction = direction.GetNextDirection(operation);
+                    }
+                    break;
+                case 'F':
+                    (int x, int y) delta = direction.GetDelta();
+                    x += delta.x * number;
+                    y += delta.y * number;
+                    break;
+                default:
+                    throw new Exception($"Found invalid operation {operation}");
+            }
+        }
+
+        return Math.Abs(x) + Math.Abs(y);
     }
 
     static long SolvePart2(string[] lines)
@@ -56,5 +93,50 @@ public class Program
         // TODO: Implement logic to solve part 2
 
         return total;
+    }
+}
+
+enum Direction
+{
+    North,
+    South,
+    West,
+    East
+}
+
+static class DirectionMethods
+{
+    public static (int x, int y) GetDelta(this Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.North:
+                return (0, -1);
+            case Direction.South:
+                return (0, 1);
+            case Direction.West:
+                return (-1, 0);
+            case Direction.East:
+                return (1, 0);
+            default:
+                throw new Exception($"Invalid directiomn given {direction}");
+        }
+    }
+
+    public static Direction GetNextDirection(this Direction direction, char operation)
+    {
+        switch (direction)
+        {
+            case Direction.North:
+                return operation == 'L' ? Direction.West : Direction.East;
+            case Direction.South:
+                return operation == 'L' ? Direction.East : Direction.West;
+            case Direction.West:
+                return operation == 'L' ? Direction.South : Direction.North;
+            case Direction.East:
+                return operation == 'L' ? Direction.North : Direction.South;
+            default:
+                throw new Exception($"Invalid directiomn given {direction}");
+        }
     }
 }
