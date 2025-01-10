a8380853ffe2f5394f1160a6edb04d735212f9cc
Jason Cleveland <jasonncleveland@gmail.com>
2025-01-10 18:01:38 -0500

    Add solution to 2020 day 18 part 1

--- 2020/day18/Program.cs
+++ 2020/day18/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 
@@ -44,7 +45,11 @@ public class Program
     {
         long total = 0;
 
-        // TODO: Implement logic to solve part 1
+        foreach (string line in lines)
+        {
+            Evaluate(line, out long result);
+            total += result;
+        }
 
         return total;
     }
@@ -54,6 +59,134 @@ public class Program
         long total = 0;
 
         // TODO: Implement logic to solve part 2
+
+        return total;
+    }
+
+    static int Evaluate(string input, out long result, int i = 0, int level = 0)
+    {
+        bool inNumber = false;
+        long previousNumber = -1;
+        char op = '\0';
+        Stack<char> stack = new();
+        for (; i < input.Length; i++)
+        {
+            char character = input[i];
+
+            if (character == '(')
+            {
+                i = Evaluate(input, out long number, i + 1, level + 1);
+                if (previousNumber != -1 && op != '\0')
+                {
+                    switch (op)
+                    {
+                        case '+':
+                            previousNumber += number;
+                            break;
+                        case '*' :
+                            previousNumber *= number;
+                            break;
+                        default:
+                            throw new Exception($"Invalid operator: {op}");
+                    }
+                    // Reset the operator
+                    op = '\0';
+                }
+                else
+                {
+                    previousNumber = number;
+                }
+                continue;
+            }
+
+            if (character == ')')
+            {
+                if (inNumber)
+                {
+                    long number = GetNumber(stack);
+                    if (previousNumber != -1 && op != '\0')
+                    {
+                        switch (op)
+                        {
+                            case '+':
+                                previousNumber += number;
+                                break;
+                            case '*' :
+                                previousNumber *= number;
+                                break;
+                            default:
+                                throw new Exception($"Invalid operator: {op}");
+                        }
+                        // Reset the operator
+                        op = '\0';
+                    }
+                    else
+                    {
+                        previousNumber = number;
+                    }
+                }
+                inNumber = false;
+                result = previousNumber;
+                return i;
+            }
+
+            if (char.IsDigit(character))
+            {
+                inNumber = true;
+                stack.Push(character);
+            }
+
+            if (char.IsWhiteSpace(character) || i == input.Length - 1)
+            {
+                if (inNumber)
+                {
+                    long number = GetNumber(stack);
+                    if (previousNumber != -1 && op != '\0')
+                    {
+                        switch (op)
+                        {
+                            case '+':
+                                previousNumber += number;
+                                break;
+                            case '*' :
+                                previousNumber *= number;
+                                break;
+                            default:
+                                throw new Exception($"Invalid operator: {op}");
+                        }
+                        // Reset the operator
+                        op = '\0';
+                    }
+                    else
+                    {
+                        previousNumber = number;
+                    }
+                }
+                inNumber = false;
+            }
+
+            if (character == '+' || character == '*')
+            {
+                op = character;
+            }
+        }
+
+        result = previousNumber;
+        return -1;
+    }
+
+    static long GetNumber(Stack<char> stack)
+    {
+        long total = 0;
+
+        total += stack.Pop() - '0';
+
+        int power = 1;
+        while (stack.Count > 0)
+        {
+            total += (stack.Pop() - '0') * (long)Math.Pow(10, power);
+            power++;
+        }
 
         return total;
     }
