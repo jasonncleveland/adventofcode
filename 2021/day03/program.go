708dd13cf53cbeba407d50c5368b98371f62d119
Jason Cleveland <jasonncleveland@gmail.com>
2025-03-29 16:26:45 -0400

    Add solution to 2021 day 3 part 2

--- 2021/day03/program.go
+++ 2021/day03/program.go
@@ -22,6 +22,10 @@ func main() {
 	start = time.Now()
 	part1 := Part1(bits)
 	fmt.Printf("Part 1: %d (%s)\n", part1, time.Since(start))
+
+	start = time.Now()
+	part2 := Part2(bits)
+	fmt.Printf("Part 2: %d (%s)\n", part2, time.Since(start))
 }
 
 func Part1(bits [][]int64) int64 {
@@ -71,6 +75,102 @@ func Part1(bits [][]int64) int64 {
 		epsilonRate += value << (lineLength - 1 - index)
 	}
 	return gammaRate * epsilonRate
+}
+
+func Part2(bits [][]int64) int64 {
+	lineLength := len(bits[0])
+
+	var oxygenGeneratorPossibleValues [][]int64
+	var co2ScrubberPossibleValues [][]int64
+
+	for _, value := range bits {
+		oxygenGeneratorPossibleValues = append(oxygenGeneratorPossibleValues, value)
+		co2ScrubberPossibleValues = append(co2ScrubberPossibleValues, value)
+	}
+
+	var commonBytes map[int64]int
+
+	for column := range lineLength {
+		// Find value for oxygen generator
+		if len(oxygenGeneratorPossibleValues) > 1 {
+			commonBytes = map[int64]int{
+				int64(0): 0,
+				int64(1): 0,
+			}
+
+			for row := range len(oxygenGeneratorPossibleValues) {
+				value := oxygenGeneratorPossibleValues[row][column]
+				commonBytes[value] += 1
+			}
+
+			var maxValue int64
+			maxCount := math.MinInt64
+
+			for value, count := range commonBytes {
+				if count > maxCount {
+					maxCount = count
+					maxValue = value
+				} else if count == maxCount {
+					// If the count is the same we need to default to 1
+					maxValue = 1
+				}
+			}
+
+			var filteredPossibleValues [][]int64
+			for row := range len(oxygenGeneratorPossibleValues) {
+				if oxygenGeneratorPossibleValues[row][column] == maxValue {
+					filteredPossibleValues = append(filteredPossibleValues, oxygenGeneratorPossibleValues[row])
+				}
+			}
+			oxygenGeneratorPossibleValues = filteredPossibleValues
+		}
+
+		// Find value for CO2 scrubber
+		if len(co2ScrubberPossibleValues) > 1 {
+			commonBytes = map[int64]int{
+				int64(0): 0,
+				int64(1): 0,
+			}
+
+			for row := range len(co2ScrubberPossibleValues) {
+				value := co2ScrubberPossibleValues[row][column]
+				commonBytes[value] += 1
+			}
+
+			var minValue int64
+			minCount := math.MaxInt64
+
+			for value, count := range commonBytes {
+				if count < minCount {
+					minCount = count
+					minValue = value
+				} else if count == minCount {
+					// If the count is the same we need to default to 0
+					minValue = 0
+				}
+			}
+
+			var filteredPossibleValues [][]int64
+			for row := range len(co2ScrubberPossibleValues) {
+				if co2ScrubberPossibleValues[row][column] == minValue {
+					filteredPossibleValues = append(filteredPossibleValues, co2ScrubberPossibleValues[row])
+				}
+			}
+			co2ScrubberPossibleValues = filteredPossibleValues
+		}
+	}
+
+	oxygenGeneratorRating := int64(0)
+	for index, value := range oxygenGeneratorPossibleValues[0] {
+		oxygenGeneratorRating += value << (lineLength - 1 - index)
+	}
+
+	co2ScrubberRating := int64(0)
+	for index, value := range co2ScrubberPossibleValues[0] {
+		co2ScrubberRating += value << (lineLength - 1 - index)
+	}
+
+	return oxygenGeneratorRating * co2ScrubberRating
 }
 
 func ParseInput(lines [][]byte) [][]int64 {
--- 2021/day03/program_test.go
+++ 2021/day03/program_test.go
@@ -27,6 +27,29 @@ func TestPart1(t *testing.T) {
 	}
 }
 
+func TestPart2(t *testing.T) {
+	bits := [][]int64{
+		{0, 0, 1, 0, 0},
+		{1, 1, 1, 1, 0},
+		{1, 0, 1, 1, 0},
+		{1, 0, 1, 1, 1},
+		{1, 0, 1, 0, 1},
+		{0, 1, 1, 1, 1},
+		{0, 0, 1, 1, 1},
+		{1, 1, 1, 0, 0},
+		{1, 0, 0, 0, 0},
+		{1, 1, 0, 0, 1},
+		{0, 0, 0, 1, 0},
+		{0, 1, 0, 1, 0},
+	}
+	result := Part2(bits)
+
+	expected := int64(230)
+	if result != expected {
+		t.Errorf("Error was incorrect, got: %d, want: %d.\n", result, expected)
+	}
+}
+
 func TestParseInput(t *testing.T) {
 	bits := [][]int64{
 		{0, 0, 1, 0, 0},
