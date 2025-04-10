dde73ef265481a26e8780e84195cfc099c21519d
Jason Cleveland <jasonncleveland@gmail.com>
2025-04-10 10:17:47 -0400

    Add template for 2021 day 14

---
+++ 2021/day14/program.go
@@ -0,0 +1,44 @@
+package day14
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/jasonncleveland/adventofcode/2021/utils"
+)
+
+func Run(fileName string) {
+	var start time.Time = time.Now()
+	lines := utils.ReadFileLines(fileName)
+	fmt.Printf("File read: %s\n", time.Since(start))
+
+	start = time.Now()
+	part1 := Part1(lines)
+	fmt.Printf("Part 1: %d (%s)\n", part1, time.Since(start))
+
+	start = time.Now()
+	part2 := Part2(lines)
+	fmt.Printf("Part 2: %d (%s)\n", part2, time.Since(start))
+}
+
+func Part1(lines [][]byte) int64 {
+	return -1
+}
+
+func Part2(lines [][]byte) int64 {
+	return -1
+}
+
+func ParseInput(lines [][]byte) [][]int64 {
+	var data [][]int64
+
+	for _, line := range lines {
+		var bytes []int64
+		for _, bit := range line {
+			bytes = append(bytes, int64(bit-byte('0')))
+		}
+		data = append(data, bytes)
+	}
+
+	return data
+}
