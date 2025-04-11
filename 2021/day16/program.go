c6184c956a1b35ea0842a25b0260b0c6b67a4378
Jason Cleveland <jasonncleveland@gmail.com>
2025-04-11 14:00:51 -0400

    Add solution to 2021 day 16 part 1

--- 2021/day16/program.go
+++ 2021/day16/program.go
@@ -2,6 +2,8 @@ package day16
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/jasonncleveland/adventofcode/2021/utils"
@@ -22,23 +24,81 @@ func Run(fileName string) {
 }
 
 func Part1(lines [][]byte) int64 {
-	return -1
+	binaryString := ConvertHexToBinary(lines[0])
+
+	var index int = 0
+	return ParsePacket(binaryString, &index)
 }
 
 func Part2(lines [][]byte) int64 {
 	return -1
 }
 
-func ParseInput(lines [][]byte) [][]int64 {
-	var data [][]int64
-
-	for _, line := range lines {
-		var bytes []int64
-		for _, bit := range line {
-			bytes = append(bytes, int64(bit-byte('0')))
-		}
-		data = append(data, bytes)
+func ConvertHexToBinary(hexString []byte) string {
+	lookupTable := map[byte]string{
+		'0': "0000",
+		'1': "0001",
+		'2': "0010",
+		'3': "0011",
+		'4': "0100",
+		'5': "0101",
+		'6': "0110",
+		'7': "0111",
+		'8': "1000",
+		'9': "1001",
+		'A': "1010",
+		'B': "1011",
+		'C': "1100",
+		'D': "1101",
+		'E': "1110",
+		'F': "1111",
 	}
 
-	return data
+	var builder strings.Builder
+	for _, hex := range hexString {
+		builder.WriteString(lookupTable[hex])
+	}
+	return builder.String()
+}
+
+func ParsePacket(packet string, index *int) int64 {
+	version, _ := strconv.ParseInt(packet[*index:*index+3], 2, 8)
+	*index += 3
+	typeId, _ := strconv.ParseInt(packet[*index:*index+3], 2, 8)
+	*index += 3
+
+	total := version
+	switch typeId {
+	case 4:
+		// Literal value
+		var result strings.Builder
+		for {
+			group := packet[*index : *index+5]
+			*index += 5
+			result.WriteString(group[1:])
+			if group[0] == '0' {
+				break
+			}
+		}
+		strconv.ParseInt(result.String(), 2, 64)
+	default:
+		// Sub-packets
+		lengthTypeId := packet[*index] - '0'
+		*index++
+		if lengthTypeId == 0 {
+			bitLength, _ := strconv.ParseInt(packet[*index:*index+15], 2, 64)
+			*index += 15
+			finalIndex := *index + int(bitLength)
+			for *index < finalIndex {
+				total += ParsePacket(packet, index)
+			}
+		} else {
+			subPacketCount, _ := strconv.ParseInt(packet[*index:*index+11], 2, 64)
+			*index += 11
+			for range subPacketCount {
+				total += ParsePacket(packet, index)
+			}
+		}
+	}
+	return total
 }
--- 2021/day16/program_test.go
+++ 2021/day16/program_test.go
@@ -5,22 +5,45 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	lines := [][]byte{
-		[]byte("00000"),
-		[]byte("00100"),
-		[]byte("01110"),
-		[]byte("00100"),
-		[]byte("00000"),
+	tests := []struct {
+		input    [][]byte
+		expected int64
+	}{
+		{
+			[][]byte{
+				[]byte("8A004A801A8002F478"),
+			},
+			16,
+		},
+		{
+			[][]byte{
+				[]byte("620080001611562C8802118E34"),
+			},
+			12,
+		},
+		{
+			[][]byte{
+				[]byte("C0015000016115A2E0802F182340"),
+			},
+			23,
+		},
+		{
+			[][]byte{
+				[]byte("A0016C880162017C3686B18A3D4780"),
+			},
+			31,
+		},
 	}
 
-	result := Part1(lines)
-	if result == -1 {
-		t.Fatal("Function or tests for part 1 not implemented")
-	}
+	for _, test := range tests {
+		result := Part1(test.input)
+		if result == -1 {
+			t.Fatal("Function or tests for part 1 not implemented")
+		}
 
-	expected := int64(0)
-	if result != expected {
-		t.Errorf("Result was incorrect, got: %v, want: %v.\n", result, expected)
+		if result != test.expected {
+			t.Errorf("Result was incorrect, got: %d, want: %d.\n", result, test.expected)
+		}
 	}
 }
 
@@ -41,37 +64,5 @@ func TestPart2(t *testing.T) {
 	expected := int64(0)
 	if result != expected {
 		t.Errorf("Result was incorrect, got: %v, want: %v.\n", result, expected)
-	}
-}
-
-func TestParseInput(t *testing.T) {
-	expected := [][]int64{
-		{0, 0, 0, 0, 0},
-		{0, 0, 1, 0, 0},
-		{0, 1, 1, 1, 0},
-		{0, 0, 1, 0, 0},
-		{0, 0, 0, 0, 0},
-	}
-	lines := [][]byte{
-		[]byte("00000"),
-		[]byte("00100"),
-		[]byte("01110"),
-		[]byte("00100"),
-		[]byte("00000"),
-	}
-
-	result := ParseInput(lines)
-	if len(result) != len(expected) {
-		t.Fatalf("Length of result is not equal to the expected length, got: %d, want %d\n", len(result), len(expected))
-	}
-	for i := range result {
-		if len(result[i]) != len(expected[i]) {
-			t.Fatalf("Length of result is not equal to the expected length, got: %d, want %d\n", len(result[i]), len(expected[i]))
-		}
-		for j := range result[i] {
-			if result[i][j] != expected[i][j] {
-				t.Errorf("Value at index %d in result is not equal to expected, got: %d, want: %d\n", i, result[i][j], expected[i][j])
-			}
-		}
 	}
 }
