4b3c19ec8da739ed7654b35d371cbdc4732d9f01
Jason Cleveland <jasonncleveland@gmail.com>
2025-04-11 21:58:55 -0400

    Add support for parsing negative numbers

--- 2021/utils/parsers.go
+++ 2021/utils/parsers.go
@@ -1,11 +1,18 @@
 package utils
 
 func ParseNumber(bytes []byte) int64 {
-	magnitude := int64(1)
-	number := int64(0)
+	var magnitude int64 = 1
+	var sign int64 = 1
+	var number int64 = 0
+
+	if bytes[0] == '-' {
+		sign = -1
+		bytes = bytes[1:]
+	}
+
 	for index := range bytes {
 		number += int64(bytes[len(bytes)-1-index]-byte('0')) * magnitude
 		magnitude *= 10
 	}
-	return number
+	return number * sign
 }
--- 2021/utils/parsers_test.go
+++ 2021/utils/parsers_test.go
@@ -2,9 +2,19 @@ package utils
 
 import "testing"
 
-func TestParseNumber(t *testing.T) {
+func TestParseNumberPositive(t *testing.T) {
 	var expected int64 = 123456
 	var input []byte = []byte("123456")
+
+	result := ParseNumber(input)
+	if result != expected {
+		t.Errorf("Result not equal to expected, got: %d, want %d\n", result, expected)
+	}
+}
+
+func TestParseNumberNegative(t *testing.T) {
+	var expected int64 = -123456
+	var input []byte = []byte("-123456")
 
 	result := ParseNumber(input)
 	if result != expected {
