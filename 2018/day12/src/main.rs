8e62168e281682742dbb08fff14c2816bd5cd9f1
Jason Cleveland <jasonncleveland@gmail.com>
2025-07-18 10:09:19 -0400

    Add template for 2018 day 12

---
+++ 2018/day12/src/main.rs
@@ -0,0 +1,77 @@
+use std::env;
+use std::fs;
+use std::time;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("Must pass filename as argument");
+    }
+
+    let input_timer = time::Instant::now();
+    let file_name = &args[1];
+    let file_contents = read_file(file_name);
+    println!("File read: ({:?})", input_timer.elapsed());
+
+    let part1_timer = time::Instant::now();
+    let part1 = part1(&file_contents);
+    println!("Part 1: {} ({:?})", part1, part1_timer.elapsed());
+
+    let part2_timer = time::Instant::now();
+    let part2 = part2(&file_contents);
+    println!("Part 2: {} ({:?})", part2, part2_timer.elapsed());
+}
+
+fn read_file(file_name: &str) -> String {
+    fs::read_to_string(file_name)
+        .expect("Something went wrong reading the file")
+}
+
+fn parse_input(file_contents: &str) -> Vec<i64> {
+    let mut numbers: Vec<i64> = Vec::new();
+    for line in file_contents.lines() {
+        numbers.push(line.parse::<i64>().unwrap());
+    }
+    numbers
+}
+
+fn part1(file_contents: &str) -> i64 {
+    -1
+}
+
+fn part2(file_contents: &str) -> i64 {
+    -1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input: [&str; 1] = [
+            "",
+        ];
+        let expected: [i64; 1] = [
+            0,
+        ];
+
+        for i in 0..input.len() {
+            assert_eq!(part1(input[i]), expected[i]);
+        }
+    }
+
+    #[test]
+    fn test_part2() {
+        let input: [&str; 1] = [
+            "",
+        ];
+        let expected: [i64; 1] = [
+            0,
+        ];
+
+        for i in 0..input.len() {
+            assert_eq!(part2(input[i]), expected[i]);
+        }
+    }
+}
