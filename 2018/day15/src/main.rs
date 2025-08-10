22bf4c057aa7d76a90ea44320d85ed2b58bb6400
Jason Cleveland <jasonncleveland@gmail.com>
2025-08-10 13:25:29 -0400

    Add solution to 2018 day 15 part 2

--- 2018/day15/src/main.rs
+++ 2018/day15/src/main.rs
@@ -60,27 +60,39 @@ fn parse_input(file_contents: &str) -> (HashMap<Coordinate, char>, HashMap<Coord
 fn part1(file_contents: &str) -> i64 {
     let (mut battlefield, mut units) = parse_input(file_contents);
 
-    let mut round = 1;
-    loop {
-        if let Some(result) = process_round(&mut battlefield, &mut units) && result {
-            if let Some(hp_remaining) = units.values().map(|u| u.hp).reduce(|a, b| a + b) {
-                return (round - 1) * hp_remaining;
-            }
-        } else {
-            let goblin_count = units.iter().filter(|(_, u)| u.faction == 'G').count();
-            let elf_count = units.iter().filter(|(_, u)| u.faction == 'E').count();
-            if elf_count == 0 || goblin_count == 0 {
-                if let Some(hp_remaining) = units.values().map(|u| u.hp).reduce(|a, b| a + b) {
-                    return round * hp_remaining;
-                }
-            }
-        }
-        round += 1;
-    }
+    simulate_battle(&mut battlefield, &mut units, 3)
 }
 
 fn part2(file_contents: &str) -> i64 {
-    -1
+    let (battlefield, units) = parse_input(file_contents);
+    let starting_elf_count = units.iter().filter(|(_, u)| u.faction == 'E').count();
+
+    let mut elf_power = 4;
+    let mut highest_elf_power_with_death = 3;
+    let mut lowest_elf_power_without_death = 200;
+    let mut results: HashMap<i64, i64> = HashMap::new();
+    loop {
+        let mut battlefield_copy = battlefield.clone();
+        let mut units_copy = units.clone();
+        let result = simulate_battle(&mut battlefield_copy, &mut units_copy, elf_power);
+        results.insert(elf_power, result);
+
+        // Use binary search to minimize the number of simulations performed
+        let elf_count = units_copy.iter().filter(|(_, u)| u.faction == 'E').count();
+        if elf_count == starting_elf_count {
+            lowest_elf_power_without_death = elf_power;
+        } else {
+            highest_elf_power_with_death = elf_power;
+        }
+        let difference = lowest_elf_power_without_death - highest_elf_power_with_death;
+        if difference == 0 || difference == 1 {
+            // If the difference is 0 or 1 then we have found the correct value
+            if let Some(result) = results.get(&lowest_elf_power_without_death) {
+                return *result;
+            }
+        }
+        elf_power = highest_elf_power_with_death + difference / 2;
+    }
 }
 
 fn print_battlefield(battlefield: &HashMap<Coordinate, char>) {
@@ -107,6 +119,22 @@ fn print_units(units: &HashMap<Coordinate, Unit>) {
     }
 }
 
+fn simulate_battle(battlefield: &mut HashMap<Coordinate, char>, units: &mut HashMap<Coordinate, Unit>, elf_power: i64) -> i64 {
+    // Set the attack power for all elf units
+    units.iter_mut().filter(|(_, u)| u.faction == 'E').for_each(|(_, u)| u.power = elf_power);
+
+    let mut round = 1;
+    loop {
+        if let Some(result) = process_round(battlefield, units) && result {
+            // If a unit is unable to find a move then the last completed round is the previous round
+            if let Some(hp_remaining) = units.values().map(|u| u.hp).reduce(|a, b| a + b) {
+                return (round - 1) * hp_remaining;
+            }
+        }
+        round += 1;
+    }
+}
+
 fn process_round(battlefield: &mut HashMap<Coordinate, char>, units: &mut HashMap<Coordinate, Unit>) -> Option<bool> {
     let units_copy = units.clone();
     let mut coordinates = units_copy.keys().collect::<Vec<&Coordinate>>();
@@ -118,14 +146,15 @@ fn process_round(battlefield: &mut HashMap<Coordinate, char>, units: &mut HashMa
             let mut unit = result.clone();
             // Movement phase
             if !in_range_of_target(battlefield, &unit) {
-                if let Some(target) = find_target(battlefield, &unit) {
-                    let next_step = target.1;
+                if let Some(next_step) = find_target(battlefield, &unit) {
+                    // Move towards the nearest target
                     battlefield.insert(next_step, unit.faction);
                     battlefield.insert(*coordinate, '.');
                     unit.position = next_step;
                     units.remove(coordinate);
                     units.insert(next_step, unit.clone());
                 } else {
+                    // If a unit cannot find a target, check and stop if all targets have been eliminated
                     let goblin_count = units.iter().filter(|(_, u)| u.faction == 'G').count();
                     let elf_count = units.iter().filter(|(_, u)| u.faction == 'E').count();
                     if elf_count == 0 || goblin_count == 0 {
@@ -135,39 +164,17 @@ fn process_round(battlefield: &mut HashMap<Coordinate, char>, units: &mut HashMa
             }
 
             // Attack phase
-            if in_range_of_target(battlefield, &unit) {
-                if let Some(target_coordinate) = get_next_target(battlefield, units, &unit) {
-                    let target = units.get_mut(&target_coordinate)?;
-                    target.hp -= unit.power;
-                    if target.hp <= 0 {
-                        units.remove(&target_coordinate);
-                        battlefield.insert(target_coordinate, '.');
-                    }
+            if let Some(target_coordinate) = get_prioritized_target(battlefield, units, &unit) {
+                let target = units.get_mut(&target_coordinate)?;
+                target.hp -= unit.power;
+                if target.hp <= 0 {
+                    units.remove(&target_coordinate);
+                    battlefield.insert(target_coordinate, '.');
                 }
             }
         }
     }
     None
-}
-
-fn in_range_of_target(battlefield: &HashMap<Coordinate, char>, unit: &Unit) -> bool {
-    let left = (unit.position.0, unit.position.1 - 1);
-    if let Some(next_tile) = battlefield.get(&left) && *next_tile == get_opposite_faction(unit.faction) {
-        return true;
-    }
-    let right = (unit.position.0, unit.position.1 + 1);
-    if let Some(next_tile) = battlefield.get(&right) && *next_tile == get_opposite_faction(unit.faction) {
-        return true;
-    }
-    let up = (unit.position.0 - 1, unit.position.1);
-    if let Some(next_tile) = battlefield.get(&up) && *next_tile == get_opposite_faction(unit.faction) {
-        return true;
-    }
-    let down = (unit.position.0 + 1, unit.position.1);
-    if let Some(next_tile) = battlefield.get(&down) && *next_tile == get_opposite_faction(unit.faction) {
-        return true;
-    }
-    false
 }
 
 fn get_neighbouring_coordinates(coordinate: Coordinate) -> Vec<Coordinate> {
@@ -179,7 +186,17 @@ fn get_neighbouring_coordinates(coordinate: Coordinate) -> Vec<Coordinate> {
     ]
 }
 
-fn find_target(battlefield: &HashMap<Coordinate, char>, unit: &Unit) -> Option<(Coordinate, Coordinate)> {
+fn in_range_of_target(battlefield: &HashMap<Coordinate, char>, unit: &Unit) -> bool {
+    let neighbours: Vec<Coordinate> = get_neighbouring_coordinates(unit.position);
+    for coordinate in neighbours {
+        if let Some(next_tile) = battlefield.get(&coordinate) && *next_tile == get_opposite_faction(unit.faction) {
+            return true;
+        }
+    }
+    false
+}
+
+fn find_target(battlefield: &HashMap<Coordinate, char>, unit: &Unit) -> Option<Coordinate> {
     let mut targets: HashMap<Coordinate, Vec<Coordinate>> = HashMap::new();
 
     let mut queue: VecDeque<(Coordinate, Coordinate, u8)> = VecDeque::new();
@@ -189,18 +206,17 @@ fn find_target(battlefield: &HashMap<Coordinate, char>, unit: &Unit) -> Option<(
     visited.insert(unit.position);
 
     let mut max_path = u8::MAX;
-    while !queue.is_empty() {
-        let current = queue.pop_front().unwrap();
-
+    while let Some(current) = queue.pop_front() {
         if current.2 > max_path {
+            // If we have already found the shortest path length to a target then cull any longer paths
             continue;
         }
 
-        let to_check: Vec<Coordinate> = get_neighbouring_coordinates(current.0);
-
-        for coordinate in to_check {
+        let neighbours: Vec<Coordinate> = get_neighbouring_coordinates(current.0);
+        for coordinate in neighbours {
             if !visited.contains(&coordinate) && let Some(next_tile) = battlefield.get(&coordinate) {
                 if *next_tile == get_opposite_faction(unit.faction) {
+                    // If the current tile is an opponent, add the previous tile to the list of possible targets
                     match targets.contains_key(&current.0) {
                         true => {
                             targets.get_mut(&current.0)?.push(current.1);
@@ -211,6 +227,7 @@ fn find_target(battlefield: &HashMap<Coordinate, char>, unit: &Unit) -> Option<(
                     }
                     max_path = current.2;
                 } else if *next_tile == '.' {
+                    // Store the first move to help calculate the best move
                     let path_origin = match current.1 {
                         (0, 0) => coordinate,
                         origin => origin,
@@ -222,36 +239,38 @@ fn find_target(battlefield: &HashMap<Coordinate, char>, unit: &Unit) -> Option<(
         }
     }
 
+    // Find the closest target with ties broken by reading order
     let targets_clone = targets.clone();
     let mut possible_targets = targets_clone.keys().collect::<Vec<&Coordinate>>();
     possible_targets.sort();
+
     if let Some(first_target) = possible_targets.first() && let Some(possible_moves) = targets.get_mut(first_target) {
+        // If there are multiple paths to the target then take the first one in reading order
         possible_moves.sort();
         if let Some(first_coordinate) = possible_moves.first() {
-            return Some((**first_target, *first_coordinate));
+            return Some(*first_coordinate);
         }
     }
     None
 }
 
-fn get_next_target(battlefield: &HashMap<Coordinate, char>, units: &HashMap<Coordinate, Unit>, unit: &Unit) -> Option<Coordinate> {
+fn get_prioritized_target(battlefield: &HashMap<Coordinate, char>, units: &HashMap<Coordinate, Unit>, unit: &Unit) -> Option<Coordinate> {
     let mut targets: Vec<&Unit> = Vec::new();
 
-    let to_check: Vec<Coordinate> = get_neighbouring_coordinates(unit.position);
-    for coordinate in to_check {
+    let neighbours: Vec<Coordinate> = get_neighbouring_coordinates(unit.position);
+    for coordinate in neighbours {
         if let Some(next_tile) = battlefield.get(&coordinate) && *next_tile == get_opposite_faction(unit.faction) {
-            let target = units.get(&coordinate);
-            targets.push(target?);
+            targets.push(units.get(&coordinate)?);
         }
+    }
+
+    if targets.is_empty() {
+        return None;
     }
 
     // Sort targets by lowest hp then position
     targets.sort_by(|a, b| (a.hp, a.position).cmp(&(b.hp, b.position)));
-    if !targets.is_empty() {
-        let first = *targets.first()?;
-        return Some(first.position);
-    }
-    None
+    Some(targets.first()?.position)
 }
 
 fn get_opposite_faction(faction: char) -> char {
@@ -340,11 +359,59 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        let input: [&str; 1] = [
-            "",
+        let input: [&str; 6] = [
+            "#######
+#.G...#
+#...EG#
+#.#.#G#
+#..G#E#
+#.....#
+#######",
+            "#######
+#G..#E#
+#E#E.E#
+#G.##.#
+#...#E#
+#...E.#
+#######",
+            "#######
+#E..EG#
+#.#G.E#
+#E.##E#
+#G..#.#
+#..E#.#
+#######",
+            "#######
+#E.G#.#
+#.#G..#
+#G.#.G#
+#G..#.#
+#...E.#
+#######",
+            "#######
+#.E...#
+#.#..G#
+#.###.#
+#E#G#G#
+#...#G#
+#######",
+            "#########
+#G......#
+#.E.#...#
+#..##..G#
+#...##..#
+#...#...#
+#.G...G.#
+#.....G.#
+#########",
         ];
-        let expected: [i64; 1] = [
-            0,
+        let expected: [i64; 6] = [
+            4988,
+            29064,
+            31284,
+            3478,
+            6474,
+            1140,
         ];
 
         for i in 0..input.len() {
