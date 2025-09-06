746fc0ef019ddae0c22c5b47f14e1aff809e5f2f
Jason Cleveland <jasonncleveland@gmail.com>
2025-09-05 23:13:53 -0400

    Add solution to 2019 day 24 part 2

--- 2019/src/days/day24.rs
+++ 2019/src/days/day24.rs
@@ -5,6 +5,7 @@ use log::{debug, trace};
 
 use crate::shared::io::parse_char_grid;
 use crate::shared::point2d::Point2d;
+use crate::shared::point3d::Point3d;
 
 pub fn solve(file_contents: String) -> (String, String) {
     let parse_timer = Instant::now();
@@ -16,7 +17,7 @@ pub fn solve(file_contents: String) -> (String, String) {
     debug!("Part 1: {} ({:?})", part1, part1_timer.elapsed());
 
     let part2_timer = Instant::now();
-    let part2 = solve_part_2(&input);
+    let part2 = solve_part_2(&input, 200);
     debug!("Part 2: {} ({:?})", part2, part2_timer.elapsed());
 
     (part1.to_string(), part2.to_string())
@@ -49,12 +50,22 @@ fn solve_part_1(input: &HashMap<Point2d, char>) -> i64 {
     }
 }
 
-fn solve_part_2(input: &HashMap<Point2d, char>) -> i64 {
-    unimplemented!()
+fn solve_part_2(input: &HashMap<Point2d, char>, cycles: i64) -> i64 {
+    let mut area: HashMap<Point3d, char> = HashMap::new();
+    for (&point, &value) in input {
+        area.insert(Point3d::new(point.x, point.y, 0), value);
+    }
+    trace!("{:?} {}", area, area.len());
+
+    let mut total = 0;
+    for _ in 0..cycles {
+        total = process_round_expanded(&mut area);
+    }
+    total
 }
 
 fn process_round(area: &mut HashMap<Point2d, char>) {
-    let mut future = [[0; 5]; 5];
+    let mut future: HashMap<Point2d, char> = HashMap::new();
 
     for y in 0..5 {
         for x in 0..5 {
@@ -71,31 +82,270 @@ fn process_round(area: &mut HashMap<Point2d, char>) {
 
                 if c == '#' && adjacent_bugs != 1 {
                     // Bug dies unless there is exactly one other bug adjacent
-                    future[y][x] = 0;
+                    future.insert(point, '.');
                 } else if c == '.' && (adjacent_bugs == 1 || adjacent_bugs == 2) {
                     // Area becomes infested if exactly 1 or 2 bugs are adjacent
-                    future[y][x] = 1;
+                    future.insert(point, '#');
                 } else {
                     // Remains the same
-                    future[y][x] = match c {
-                        '#' => 1,
-                        '.' => 0,
-                        _ => unreachable!()
-                    };
+                    future.insert(point, c);
                 }
             }
         }
     }
 
-    for y in 0..5 {
-        for x in 0..5 {
-            let point = Point2d::new(x as i64, y as i64);
-            area.insert(point, match future[y][x] {
-                0 => '.',
-                1 => '#',
-                _ => unreachable!()
-            });
+    for (point, c) in future {
+        area.insert(point, c);
+    }
+}
+
+fn process_round_expanded(area: &mut HashMap<Point3d, char>) -> i64 {
+    let mut future: HashMap<Point3d, char> = HashMap::new();
+
+    // Find all points that need to be checked for generation
+    let mut points_to_check: HashSet<Point3d> = HashSet::new();
+    for (point, _) in area.clone() {
+        // Ignore middle point
+        if point.x == 2 && point.y == 2 {
+            continue;
         }
+
+        points_to_check.extend(get_neighbours(&point));
+        points_to_check.insert(point);
+    }
+
+    for point in points_to_check {
+        trace!("checking point: {}", point);
+
+        // Get neighbours and count bugs
+        let mut adjacent_bugs = 0;
+        let neighbours = get_neighbours(&point);
+        trace!("neighbours of {}: {:?}", point, neighbours);
+        for neighbour in neighbours {
+            trace!("value at {}: {:?}", neighbour, area.get(&neighbour));
+            if let Some(&c) = area.get(&neighbour) && c == '#' {
+                adjacent_bugs += 1;
+            }
+        }
+
+        let current_value = match area.get(&point) {
+            Some('#') => '#',
+            _ => '.'
+        };
+        if current_value == '#' {
+            // Bug dies unless there is exactly one other bug adjacent
+            if adjacent_bugs == 1 {
+                future.insert(point.clone(), '#');
+            } else {
+                future.insert(point.clone(), '.');
+            }
+        } else if current_value == '.' {
+            // Area becomes infested if exactly 1 or 2 bugs are adjacent
+            if adjacent_bugs == 1 || adjacent_bugs == 2 {
+                future.insert(point.clone(), '#');
+            } else {
+                future.insert(point.clone(), '.');
+            }
+        } else {
+            unreachable!("we should not reach here but panic if we do");
+        }
+    }
+
+    let mut total = 0;
+    for (point, c) in future {
+        if c == '#' {
+            total += 1;
+        }
+        area.insert(point, c);
+    }
+    total
+}
+
+fn get_neighbours(point: &Point3d) -> Vec<Point3d> {
+    // Inner edges
+    if point.x == 2 && point.y == 1 {
+        // (2, 1)
+        trace!("found top inner edge z: {} -> {}", point.z, point.z - 1);
+        vec![
+            // 5 top neighbours on lower level
+            Point3d::new(0, 0, point.z + 1),
+            Point3d::new(1, 0, point.z + 1),
+            Point3d::new(2, 0, point.z + 1),
+            Point3d::new(3, 0, point.z + 1),
+            Point3d::new(4, 0, point.z + 1),
+            // Left
+            Point3d::new(point.x - 1, point.y, point.z),
+            // Right
+            Point3d::new(point.x + 1, point.y, point.z),
+            // Up
+            Point3d::new(point.x, point.y - 1, point.z),
+        ]
+    } else if point.x == 2 &&  point.y == 3 {
+        // (2, 3)
+        trace!("found bottom inner edge z: {} -> {}", point.z, point.z - 1);
+        vec![
+            // 5 bottom neighbours on lower level
+            Point3d::new(0, 4, point.z + 1),
+            Point3d::new(1, 4, point.z + 1),
+            Point3d::new(2, 4, point.z + 1),
+            Point3d::new(3, 4, point.z + 1),
+            Point3d::new(4, 4, point.z + 1),
+            // Left
+            Point3d::new(point.x - 1, point.y, point.z),
+            // Right
+            Point3d::new(point.x + 1, point.y, point.z),
+            // Down
+            Point3d::new(point.x, point.y + 1, point.z),
+        ]
+    } else if point.x == 1 && point.y == 2 {
+        // (1, 2)
+        trace!("found left inner edge z: {} -> {}", point.z, point.z - 1);
+        vec![
+            // 5 right neighbours on lower level
+            Point3d::new(0, 0, point.z + 1),
+            Point3d::new(0, 1, point.z + 1),
+            Point3d::new(0, 2, point.z + 1),
+            Point3d::new(0, 3, point.z + 1),
+            Point3d::new(0, 4, point.z + 1),
+            // Left
+            Point3d::new(point.x - 1, point.y, point.z),
+            // Up
+            Point3d::new(point.x, point.y - 1, point.z),
+            // Down
+            Point3d::new(point.x, point.y + 1, point.z),
+        ]
+    } else if point.x == 3 && point.y == 2 {
+        // (3, 2)
+        trace!("found right inner edge z: {} -> {}", point.z, point.z - 1);
+        vec![
+            // 5 left neighbours on lower level
+            Point3d::new(4, 0, point.z + 1),
+            Point3d::new(4, 1, point.z + 1),
+            Point3d::new(4, 2, point.z + 1),
+            Point3d::new(4, 3, point.z + 1),
+            Point3d::new(4, 4, point.z + 1),
+            // Right
+            Point3d::new(point.x + 1, point.y, point.z),
+            // Up
+            Point3d::new(point.x, point.y - 1, point.z),
+            // Down
+            Point3d::new(point.x, point.y + 1, point.z),
+        ]
+    // Outer corners
+    } else if point.x == 0 && point. y == 0 {
+        // (0, 0)
+        trace!("found top left corner z: {} -> {}", point.z, point.z + 1);
+        vec![
+            // Left on upper level
+            Point3d::new(1, 2, point.z - 1),
+            // Up on upper level
+            Point3d::new(2, 1, point.z - 1),
+            // Right
+            Point3d::new(point.x + 1, point.y, point.z),
+            // Down
+            Point3d::new(point.x, point.y + 1, point.z),
+        ]
+    } else if point.x == 4 && point.y == 0 {
+        // (4, 0)
+        trace!("found top right corner z: {} -> {}", point.z, point.z + 1);
+        vec![
+            // Right on upper level
+            Point3d::new(3, 2, point.z - 1),
+            // Up on upper level
+            Point3d::new(2, 1, point.z - 1),
+            // Left
+            Point3d::new(point.x - 1, point.y, point.z),
+            // Down
+            Point3d::new(point.x, point.y + 1, point.z),
+        ]
+    } else if point.x == 0 && point.y == 4 {
+        // (0, 4)
+        trace!("found bottom left corner z: {} -> {}", point.z, point.z + 1);
+        vec![
+            // Left on upper level
+            Point3d::new(1, 2, point.z - 1),
+            // Down on upper level
+            Point3d::new(2, 3, point.z - 1),
+            // Right
+            Point3d::new(point.x + 1, point.y, point.z),
+            // Up
+            Point3d::new(point.x, point.y - 1, point.z),
+        ]
+    } else if point.x == 4 && point.y == 4 {
+        // (4, 4)
+        trace!("found bottom right corner z: {} -> {}", point.z, point.z + 1);
+        vec![
+            // Right on upper level
+            Point3d::new(3, 2, point.z - 1),
+            // Down on upper level
+            Point3d::new(2, 3, point.z - 1),
+            // Left
+            Point3d::new(point.x - 1, point.y, point.z),
+            // Up
+            Point3d::new(point.x, point.y - 1, point.z),
+        ]
+    // Outer edges
+    } else if point.x == 0 && point.y > 0 && point.y < 4 {
+        trace!("found left outer edge z: {} -> {}", point.z, point.z + 1);
+        vec![
+            // Left on upper level
+            Point3d::new(1, 2, point.z - 1),
+            // Right
+            Point3d::new(point.x + 1, point.y, point.z),
+            // Up
+            Point3d::new(point.x, point.y - 1, point.z),
+            // Down
+            Point3d::new(point.x, point.y + 1, point.z),
+        ]
+    } else if point.x == 4 && point.y > 0 && point.y < 4 {
+        // (0, 0)
+        trace!("found right outer edge z: {} -> {}", point.z, point.z + 1);
+        vec![
+            // Right on upper level
+            Point3d::new(3, 2, point.z - 1),
+            // Left
+            Point3d::new(point.x - 1, point.y, point.z),
+            // Up
+            Point3d::new(point.x, point.y - 1, point.z),
+            // Down
+            Point3d::new(point.x, point.y + 1, point.z),
+        ]
+    } else if point.y == 0 && point.x > 0 && point.x < 4 {
+        trace!("found top outer edge z: {} -> {}", point.z, point.z + 1);
+        vec![
+            // Top on upper level
+            Point3d::new(2, 1, point.z - 1),
+            // Left
+            Point3d::new(point.x - 1, point.y, point.z),
+            // Right
+            Point3d::new(point.x + 1, point.y, point.z),
+            // Down
+            Point3d::new(point.x, point.y + 1, point.z),
+        ]
+    } else if point.y == 4 && point.x > 0 && point.x < 4 {
+        trace!("found bottom outer edge z: {} -> {}", point.z, point.z + 1);
+        vec![
+            // Bottom on upper level
+            Point3d::new(2, 3, point.z - 1),
+            // Left
+            Point3d::new(point.x - 1, point.y, point.z),
+            // Right
+            Point3d::new(point.x + 1, point.y, point.z),
+            // Up
+            Point3d::new(point.x, point.y - 1, point.z),
+        ]
+    // Inner squares
+    } else {
+        vec![
+            // Left
+            Point3d::new(point.x - 1, point.y, point.z),
+            // Right
+            Point3d::new(point.x + 1, point.y, point.z),
+            // Up
+            Point3d::new(point.x, point.y - 1, point.z),
+            // Down
+            Point3d::new(point.x, point.y + 1, point.z),
+        ]
     }
 }
 
@@ -125,15 +375,19 @@ mod tests {
     #[test]
     fn test_part_2() {
         let input: [&str; 1] = [
-            "",
+            "....#
+#..#.
+#..##
+..#..
+#....",
         ];
         let expected: [i64; 1] = [
-            0,
+            99,
         ];
 
         for i in 0..input.len() {
             let input = parse_char_grid(input[i].to_string());
-            assert_eq!(solve_part_2(&input), expected[i]);
+            assert_eq!(solve_part_2(&input, 10), expected[i]);
         }
     }
 }
