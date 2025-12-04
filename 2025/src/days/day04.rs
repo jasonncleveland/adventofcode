80672e4fedf777782bcf5bf978c13128bdfd0338
Jason Cleveland <jasonncleveland@gmail.com>
2025-12-04 00:07:40 -0500

    Add solution to 2025 day 4 in rust

---
+++ 2025/src/days/day04.rs
@@ -0,0 +1,137 @@
+use std::collections::HashMap;
+use std::time::Instant;
+
+use aoc_helpers::grid::get_dimensions;
+use aoc_helpers::io::parse_char_grid;
+use aoc_helpers::point2d::Point2d;
+use log::debug;
+
+pub fn solve(file_contents: &str) -> (String, String) {
+    let parse_timer = Instant::now();
+    let input = parse_char_grid(file_contents);
+    debug!("File parse: ({:?})", parse_timer.elapsed());
+
+    let part1_timer = Instant::now();
+    let part1 = solve_part_1(&input);
+    debug!("Part 1: {} ({:?})", part1, part1_timer.elapsed());
+
+    let part2_timer = Instant::now();
+    let part2 = solve_part_2(&input);
+    debug!("Part 2: {} ({:?})", part2, part2_timer.elapsed());
+
+    (part1.to_string(), part2.to_string())
+}
+
+fn solve_part_1(input: &HashMap<Point2d, char>) -> i64 {
+    let mut total = 0;
+
+    for (point, &value) in input {
+        if value != '@' {
+            continue;
+        }
+
+        let mut neighbouring_rolls = 0;
+        for neighbour in point.neighbours8() {
+            if matches!(input.get(&neighbour), Some('@')) {
+                neighbouring_rolls += 1;
+            }
+        }
+        if neighbouring_rolls < 4 {
+            total += 1;
+        }
+    }
+
+    total
+}
+
+fn solve_part_2(input: &HashMap<Point2d, char>) -> i64 {
+    let mut total = 0;
+
+    let (max_x, max_y) = get_dimensions(input);
+
+    let mut input_copy = input.clone();
+    loop {
+        let mut to_remove: Vec<Point2d> = Vec::new();
+        for y in 0..max_y {
+            for x in 0..max_x {
+                let point = Point2d::new(x, y);
+                match input_copy.get(&point) {
+                    Some('@') => (),
+                    _ => continue,
+                }
+
+                let mut neighbouring_rolls = 0;
+                for neighbour in point.neighbours8() {
+                    if matches!(input_copy.get(&neighbour), Some('@')) {
+                        neighbouring_rolls += 1;
+                    }
+                }
+                if neighbouring_rolls < 4 {
+                    total += 1;
+                    to_remove.push(point);
+                }
+            }
+        }
+
+        // Stop when there are no more rolls to remove
+        if to_remove.is_empty() {
+            break;
+        }
+
+        // Remove accessible rolls
+        for point in &to_remove {
+            input_copy.remove(point);
+        }
+    }
+
+    total
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        let data: [(&str, i64); 1] = [(
+            "..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.",
+            13,
+        )];
+
+        for (input, expected) in data {
+            let input = parse_char_grid(input);
+            assert_eq!(solve_part_1(&input), expected);
+        }
+    }
+
+    #[test]
+    fn test_part_2() {
+        let data: [(&str, i64); 1] = [(
+            "..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.",
+            43,
+        )];
+
+        for (input, expected) in data {
+            let input = parse_char_grid(input);
+            assert_eq!(solve_part_2(&input), expected);
+        }
+    }
+}
--- 2025/src/days/mod.rs
+++ 2025/src/days/mod.rs
@@ -1,6 +1,7 @@
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 
 use aoc_helpers::solve::SolveFunction;
 
@@ -9,6 +10,7 @@ pub fn get_solve_module(day: u8) -> Result<SolveFunction, String> {
         1 => Ok(day01::solve),
         2 => Ok(day02::solve),
         3 => Ok(day03::solve),
+        4 => Ok(day04::solve),
         other => Err(format!("Invalid day provided: {other}")),
     }
 }
